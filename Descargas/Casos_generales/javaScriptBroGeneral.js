7d9f36bbef8da4fd4842bd76b605268959927fbe
Vectorlolo <victorbri2112@gmail.com>
2024-06-17 23:45:04 -0400

    cambios casos generales en descargas y el casos de estudio

--- Descargas/Casos_generales/javaScriptBroGeneral.js
+++ Descargas/Casos_generales/javaScriptBroGeneral.js
@@ -2,8 +2,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   var button = document.getElementById('buttonProducts');
   var button2list = document.getElementById('buttonProducts2');
+  var button3list = document.getElementById('buttonProducts3');
+
   var options = document.getElementById('optionProducts');
   var options2list = document.getElementById('optionCases');
+  var options3list = document.getElementById('optionResources');
+
   function ocultarLista() {
     options.style.display = 'none';
   }
@@ -16,11 +20,19 @@ document.addEventListener('DOMContentLoaded', function () {
   function mostrarLista2() {
     options2list.style.display = 'block';
   }
+  function ocultarLista3() {
+    options3list.style.display = 'none';
+  }
+  function mostrarLista3() {
+    options3list.style.display = 'block';
+  }
   // Agrega el evento 'mouseenter' para mostrar la lista cuando el mouse entra en el li
   button.addEventListener('mouseenter', mostrarLista);
   button.addEventListener('mouseleave', ocultarLista);
   button2list.addEventListener('mouseenter', mostrarLista2);
   button2list.addEventListener('mouseleave', ocultarLista2);
+  button3list.addEventListener('mouseenter', mostrarLista3);
+  button3list.addEventListener('mouseleave', ocultarLista3);
 
   ////////////////////////
   //Botones desplegables//
@@ -63,7 +75,6 @@ document.addEventListener('DOMContentLoaded', function () {
   var botonesDesplegable = document.querySelectorAll('.botones-desplegables');
   function mostrarOcultarContenidoDesplegables(event) {
     var boton = event.target;
-    console.log(boton);
     var targetId = boton.getAttribute('data-target');
     var contenido = document.querySelector(`[data-toggle="${targetId}"]`);
 
@@ -75,3 +86,21 @@ document.addEventListener('DOMContentLoaded', function () {
     boton.addEventListener('click', mostrarOcultarContenidoDesplegables);
   });
 });
+function descargarPDF() {
+  // Ruta del archivo PDF que quieres descargar
+  var rutaDelArchivo = '../../Assets/Descargables/BrochureCasosEstudio.pdf';
+
+  // Crear un enlace temporal a la URL
+  var tempLink = document.createElement('a');
+  tempLink.href = rutaDelArchivo;
+  tempLink.setAttribute('target', '_blank');
+
+  // Agregar el enlace temporal al DOM
+  document.body.appendChild(tempLink);
+
+  // Hacer clic en el enlace
+  tempLink.click();
+
+  // Eliminar el enlace temporal
+  document.body.removeChild(tempLink);
+}
